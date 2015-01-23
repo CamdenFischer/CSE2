45136ad0046e5be1a16e3be150c6f36ebb513ff2
camdenfischer <cwf218@lehigh.edu>
2015-01-23 16:39:56 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+// Camden Fischer
+// 1/23/15
+
+// Define a class
+public class HelloWorld{
+    //add main method
+    public static void main(String[] args) {
+        //Print the statement
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("#                     #");
+        System.out.println("#  Hello World 2015!! #");
+        System.out.println("#                     #");
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("test");
+        
+        //test/testest
+    }
+}
